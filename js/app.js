a8c3b9548a56461578d51981f65aee9453a662e3
Sophie2525 <blu.heart87@gmail.com>
2017-04-04 12:04:07 +0100

    reformatted js with collison detection added

--- js/app.js
+++ js/app.js
@@ -1,69 +1,87 @@
-$(function() {
+$(init);
 
-// Step 1: Get dustbin div to move along the bottom of the page, controlled by arrow keys.
+let difficulty = 3000;
+let $basket;
+
+function init() {
+  $basket = $('.dustbin');
+  $('button').on('click', startGame);
+}
+
+function startGame() {
+  // start timer
+  moveBasket();
+  setInterval(createBall, difficulty);
+}
+
+function moveBasket() {
   $(document).on('keydown', function move(e){
-  // $(document).keydown(function(e) {
-  // use .which to get the keycode of the arrow keys
+    // $(document).keydown(function(e) {
+    // use .which to get the keycode of the arrow keys
     switch (e.which) {
-    // move left
+      // move left
       case 37:
         $('.dustbin').stop().animate({
-          left: '-=50'
+          left: '-=100'
         });
         break;
       // move right
       case 39:
         $('.dustbin').stop().animate({
-          left: '+=50'
+          left: '+=100'
         }); //right arrow key
         break;
     }
   });
+}
 
-// NB need to make it constant
-// NB need to make it stop at edge of parent div
+function getRandomNumberFromRange() {
+  return Math.floor(Math.random() * (585 - 0 + 1)) + 0;
+}
 
-// Step 2: Drop ball from top automatically and make it loop
-  $('button').on('click', function loop(){
-  // add in function to make it loop continuously
-    $('.ball').css({top: 0});
-    $('.ball').animate({
-      top: '650px'
-    }, {
-      duration: 5000,
-      step: collisionCheck,
-      complete: loop
-    });
+function createBall() {
+  const $ball = $('<div class="ball"></div>');
+  const randomLeftValue = getRandomNumberFromRange();
+  $ball.css('left', randomLeftValue);
+  $('.container').append($ball);
+  animateBall($ball);
+}
+
+function animateBall($ball) {
+  $ball.animate({
+    top: `+=${$ball.parent().height()}px`
+  }, {
+    duration: difficulty,
+    step: () => collisionCheck($ball),
+    complete: gameOver
   });
+}
 
-// Step 3: Register score when Ball & Dustbin meet
-
-  function collisionCheck() {
-    var dustbin = getPositions(document.getElementsByClassName('dustbin')[0]);
-    var ball = getPositions(document.getElementsByClassName('ball')[0]);
-    if (dustbin.top < ball.bottom) {
-      console.log('IN THE BIIIIN');
-    }
-    // console.log(ball.bottom);
+function collisionCheck($ball) {
+  if (collision($ball)) {
+    $ball.stop().remove();
+    console.log('score');
   }
+}
 
-  function getPositions(element) {
-    return element.getBoundingClientRect();
-  }
-// Step 4: Create a level below which you lose
+function collision($ball) {
+  var x1 = $ball.offset().left;
+  var y1 = $ball.offset().top;
+  var h1 = $ball.outerHeight(true);
+  var w1 = $ball.outerWidth(true);
+  var b1 = y1 + h1;
+  var r1 = x1 + w1;
+  var x2 = $basket.offset().left;
+  var y2 = $basket.offset().top;
+  var h2 = $basket.outerHeight(true);
+  var w2 = $basket.outerWidth(true);
+  var b2 = y2 + h2;
+  var r2 = x2 + w2;
 
-// Step 5: Create 20 balls all of which register when catch them
+  if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) return false;
+  return true;
+}
 
-// function newFootballs(){
-//
-// }
-
-// Step 6: Create timer so have 2 minutes to catch balls
-// Step 7: Create start to game that triggers timer
-
-// Step 8: Next level - make them come down faster
-
-
-
-
-});
+function gameOver() {
+  console.log('gameover');
+}
