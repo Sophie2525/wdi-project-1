8126319db2d04c0f835ea8f31c6ca9aadeb33092
Sophie2525 <blu.heart87@gmail.com>
2017-04-05 16:07:03 +0100

    removed gameover when game has been completed

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 $(init);
 
-const difficulty = 3000;
+const difficulty = 2500;
 let $basket;
 let $score = 0;
 let $time = 60;
@@ -11,17 +11,26 @@ function init() {
 }
 
 function startGame() {
+  $time = 60;
+  $score = 0;
+  $('.score').text('');
   timer();
   moveBasket();
-  setInterval(createBall, difficulty);
+  var createBalls = setInterval(createBall, difficulty);
 
   const counter = setInterval(timer, 1000);
+
   function timer() {
     $time -= 1;
     // console.log($time);
+    // nextLevel();
     if ($time === 0) {
       clearInterval(counter);
-      alert('You scored' + '' + $score);
+      clearInterval(createBalls);
+      $('.ball').remove().stop();
+      console.log('You scored' + ' ' + $score);
+      // startGame();
+      // document.location.reload();
     }
     $('.timer').html($time);
   }
@@ -80,22 +89,30 @@ function collisionCheck($ball) {
 }
 
 function collision($ball) {
-  var x1 = $ball.offset().left;
-  var y1 = $ball.offset().top;
-  var h1 = $ball.outerHeight(true);
-  var w1 = $ball.outerWidth(true);
-  var b1 = y1 + h1;
-  var r1 = x1 + w1;
-  var x2 = $basket.offset().left;
-  var y2 = $basket.offset().top;
-  var h2 = $basket.outerHeight(true);
-  var w2 = $basket.outerWidth(true);
-  var b2 = y2 + h2;
-  var r2 = x2 + w2;
+  const x1 = $ball.offset().left;
+  const y1 = $ball.offset().top;
+  const h1 = $ball.outerHeight(true);
+  const w1 = $ball.outerWidth(true);
+  const b1 = y1 + h1;
+  const r1 = x1 + w1;
+  const x2 = $basket.offset().left;
+  const y2 = $basket.offset().top;
+  const h2 = $basket.outerHeight(true);
+  const w2 = $basket.outerWidth(true);
+  const b2 = y2 + h2;
+  const r2 = x2 + w2;
 
   if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) return false;
   return true;
 }
+
+// function nextLevel(){
+//   if (($time === 0) && ($score > 15)){
+//     alert( 'next level');
+//   } else if (($time === 0) & ($score <= 15)){
+//     alert('Game Over!');
+//   }
+// }
 
 function gameOver() {
   alert('Game Over!');
